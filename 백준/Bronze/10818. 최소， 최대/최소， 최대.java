bde52be43de0ad860b38cdc327e4bcb2aceef9d6
SUNGKYU KIM <seooonggyu@gmail.com>
2026-02-12 15:47:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 88012 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int min = 1000000;
+        int max = -1000000;
+        int tmp;
+
+        for(int i = 0; i < input; i++) {
+            tmp = Integer.parseInt(st.nextToken());
+            if(tmp < min) {
+                min = tmp;
+            }
+            if(tmp > max) {
+                max = tmp;
+            }
+        }
+
+        System.out.println(min + " " +  max);
+    }
+}
