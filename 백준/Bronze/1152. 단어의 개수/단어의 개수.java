63f1ddcb40e8cbe64a011879858efd9f096b4729
SUNGKYU KIM <seooonggyu@gmail.com>
2026-02-12 16:09:00 +0900

    [Bronze II] Title: 단어의 개수, Time: 192 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,12 +1,29 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        System.out.println(st.countTokens());
+        String input = br.readLine();
+        int count = 0;
+
+        if(input.length()==1) {
+            if(input.charAt(0)!=' ') {count++;}
+        } else {
+            for(int i = 0; i < input.length(); i++) {
+                if(input.charAt(i) != ' ') {
+                    if(i==0){
+                        count++;
+                    } else {
+                        if (input.charAt(i - 1) == ' ') {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
