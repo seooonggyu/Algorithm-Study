4020fa619270a242c1264cdd99b3062485939ba8
SUNGKYU KIM <seooonggyu@gmail.com>
2026-03-05 15:16:36 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,12 +10,14 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         String input = br.readLine();
         //StringTokenizer st = new StringTokenizer(br.readLine());
-        double result = 0;
-
+        long result = 0;
+        long tmp = 1;
         for (int i = 0; i < N; i++) {
-            result = (result + (input.charAt(i) - 96) * Math.pow(31, i));
+            result = (result + (input.charAt(i) - 'a'+1) * tmp) %1234567891;
+            tmp = tmp * 31 %1234567891;
         }
 
-        System.out.println((int)result);
+
+        System.out.println(result);
     }
 }
