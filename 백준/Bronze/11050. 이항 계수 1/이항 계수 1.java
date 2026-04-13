7ece4bbbabf9601389c5205b673864ec843dac30
SUNGKYU KIM <seooonggyu@gmail.com>
2026-04-13 10:15:12 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(N)/(factorial(K) * factorial(N-K)));
+    }
+
+    private static int factorial(int n) {
+        if(n<=1) return 1;
+        return n*factorial(n-1);
+    }
+}
